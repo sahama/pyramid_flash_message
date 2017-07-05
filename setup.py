797b330945402e49a49f041c8e0d4811a6e2c2d3
me <s.h.mahdavi@chmail.ir>
2017-07-05 16:38:15 +0430

    version 0.2.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires = [
 
 
 setup(name='pyramid_flash_message',
-      version='0.2.3.dev',
+      version='0.2.3',
       description='Small tool to add and show flash messages',
       long_description=README,
       classifiers=[
